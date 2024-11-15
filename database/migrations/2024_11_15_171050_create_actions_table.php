71b1eb8f11e1f6146b5108ac13523332b383c815
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-11-15 19:00:12 +0100

    IMPROVE ⭐ Action table

--- database/migrations/2024_11_15_171050_create_actions_table.php
+++ database/migrations/2024_11_15_171050_create_actions_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('actions', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('user_id');
+            $table->string('name');
+            $table->text('description');
+
             $table->timestamps();
         });
     }
