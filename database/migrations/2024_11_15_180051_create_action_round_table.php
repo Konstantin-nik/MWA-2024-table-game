800f1960a01dff9c31b3048e1d250768b97dc66c
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-11-15 19:02:27 +0100

    FEATURE ✨ action_round relation table

---
+++ database/migrations/2024_11_15_180051_create_action_round_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('action_round', function (Blueprint $table) {
+            $table->foreignId('round_id');
+            $table->foreignId('action_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('action_round');
+    }
+};
