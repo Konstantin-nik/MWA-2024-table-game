498cb05074ffa2f31a361335aad96b50d603af51
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-11-18 18:40:07 +0100

    DESIGN 🎨 add accent on hower on

--- resources/views/user/rooms/index.blade.php
+++ resources/views/user/rooms/index.blade.php
@@ -4,8 +4,8 @@
         
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($rooms as $room)
-            <a href="{{ route('user.rooms.show', $room->id) }}" >
-                <div class="bg-white shadow-md rounded-lg p-4 border border-gray-200">
+            <a href="{{ route('user.rooms.show', $room->id) }}" class="transform hover:scale-105 transition-transform duration-200">
+                <div class="bg-white shadow-md rounded-lg p-4 border border-gray-200 hover:border-blue-500 hover:shadow-lg">
                     <h3 class="text-lg font-semibold text-gray-700 mb-2">{{ $room->name }} Room</h3>
                     
                     <div class="flex items-center justify-between text-gray-500 text-sm mb-4">
