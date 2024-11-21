6dde0d53c939e53f5e6f5d87350c4c0ec6b6d7d8
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-11-21 09:13:55 +0100

    IMPROVE ⭐ add Seeding to test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Participation;
 use App\Models\Room;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -24,9 +26,26 @@ class DatabaseSeeder extends Seeder
             $room->users()->attach($list_of_users);
         }
 
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        $room = Room::create([
+            'owner_id' => 3,
+            'name' => 'Old Room',
+            'capacity' => 3,
+            'is_public' => true,
+            'invitation_token' => 'Y3kbJsi3',
+            'started_at' => now()->addDays(-2),
+            'finished_at' => now()->addDays(-2),
+        ]);
+
+        Participation::create([
+            'user_id' => $user->id,
+            'room_id' => $room->id,
+            'score' => 80,
+            'rank' => 1
         ]);
     }
 }
