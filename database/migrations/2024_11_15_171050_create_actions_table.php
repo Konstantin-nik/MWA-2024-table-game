4e740a09dedd48f6ab4c1803ef036c031e5bb18b
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-11-26 19:19:53 +0100

    IMPROVE ⭐ update action table

--- database/migrations/2024_11_15_171050_create_actions_table.php
+++ database/migrations/2024_11_15_171050_create_actions_table.php
@@ -14,9 +14,12 @@ return new class extends Migration
         Schema::create('actions', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId('user_id');
-            $table->string('name');
-            $table->text('description');
+            $table->foreignId('round_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->integer('chosen_deck'); // Deck index: 0, 1, or 2
+            $table->string('chosen_action'); // The action chosen
+            $table->integer('chosen_number'); // The house number chosen
+            $table->json('action_details')->nullable(); // Extra details (e.g., houses modified)
 
             $table->timestamps();
         });
