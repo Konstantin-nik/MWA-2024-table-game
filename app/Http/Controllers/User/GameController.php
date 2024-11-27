66c03b0b1ad11f25ca0f200af9ce9445b30d7034
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-11-27 20:33:16 +0100

    FIX 🐛 not fetch data if game does not exist

--- app/Http/Controllers/User/GameController.php
+++ app/Http/Controllers/User/GameController.php
@@ -11,18 +11,21 @@ class GameController extends Controller
     public function show()
     {
         $room = auth()->user()->getCurrentGame();
+        if ($room) {
+            $participation = $room->participations()->where("user_id", auth()->user()->id)->first();
 
-        $participation = $room->participations()->where("user_id", auth()->user()->id)->first();
+            $board = Board::with(['rows.houses'])->where('participation_id', $participation->id)->first();
 
-        $board = Board::with(['rows.houses'])->where('participation_id', $participation->id)->first();
+            $decks = Deck::with('cards')->where('room_id', $room->id)->get();
 
-        $decks = Deck::with('cards')->where('room_id', $room->id)->get();
-
-        return view('user.game', [
-            'room' => $room,
-            'participation' => $participation,
-            'board' => $board,
-            'decks' => $decks,
-        ]);
+            return view('user.game', [
+                'room' => $room,
+                'participation' => $participation,
+                'board' => $board,
+                'decks' => $decks,
+            ]);
+        } else {
+            return view('user.game', compact('room'));
+        }
     }
 }
