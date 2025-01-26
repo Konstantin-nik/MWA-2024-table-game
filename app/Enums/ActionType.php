f5bcfcb7c2621e0e604cb8c44561e53fc24e63ba
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2025-01-26 19:52:32 +0300

    REFACTOR 🔨 add ActionType and constants

---
+++ app/Enums/ActionType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum ActionType
+{
+    case FENCE = 1;
+    case ESTATE = 2;
+    case LANDSCAPE = 3;
+    case POOL = 4;
+    case AGENCY = 5;
+    case BIS = 6;
+}
