8e2ac70a4bb9342e320cc557457f525c7a493a38
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2025-01-29 19:20:06 +0300

    REFACTOR 🔨 cleanup

--- routes/console.php
+++ routes/console.php
@@ -5,4 +5,3 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('php artisan backup:clean')->dailyAt('03:10')->timezone('Europe/Madrid');
 Schedule::command('backup:run --only-db')->dailyAt('03:15')->timezone('Europe/Madrid');
 Schedule::command('backup:run')->sundays()->at('03:20')->timezone('Europe/Madrid');
-
